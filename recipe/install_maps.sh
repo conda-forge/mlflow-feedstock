517668125cdab806ea4f021038cf9f9248b6c4c2
Uwe L. Korn <uwe.korn@quantco.com>
2019-07-05 13:23:55 +0200

    MNT: Re-rendered with conda-build 3.18.6, conda-smithy 3.4.0, and conda-forge-pinning 2019.07.01

--- recipe/install_maps.sh
+++ recipe/install_maps.sh
@@ -2,7 +2,6 @@
 
 set -euo pipefail
 
-ls -l
 mkdir -p $PREFIX/lib/python${PY_VER}/site-packages/mlflow/server/js/build/static/css/
 cp $SRC_DIR/mlflow/server/js/build/static/css/*.css.map $PREFIX/lib/python${PY_VER}/site-packages/mlflow/server/js/build/static/css/
 mkdir -p $PREFIX/lib/python${PY_VER}/site-packages/mlflow/server/js/build/static/js/
