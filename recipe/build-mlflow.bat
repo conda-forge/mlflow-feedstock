ab483e56ff8e601417319b9312bd1c8b16ea0402
Uwe L. Korn <uwe.korn@quantco.com>
2021-05-17 17:31:29 +0200

    Fix mlflow-skinny on Windows

--- recipe/build-mlflow.bat
+++ recipe/build-mlflow.bat
@@ -1,6 +1,14 @@
 @echo on
+
+if [%PKG_NAME%] == [mlflow-skinny] (
+  set MLFLOW_SKINNY=1
+  # https://github.com/mlflow/mlflow/pull/4134
+  copy %RECIPE_DIR%/README_SKINNY.rst %SRC_DIR%
+)
+
 %PREFIX%/python.exe -m pip install . --no-deps --ignore-installed -vv
+
 if [%PKG_NAME%] == [mlflow] (
-bash -c 'rm ${PREFIX//\\\\//}/Lib/site-packages/mlflow/server/js/build/static/css/*.css.map'
-bash -c 'rm ${PREFIX//\\\\//}/Lib/site-packages/mlflow/server/js/build/static/js/*.js.map'
+  bash -c 'rm ${PREFIX//\\\\//}/Lib/site-packages/mlflow/server/js/build/static/css/*.css.map'
+  bash -c 'rm ${PREFIX//\\\\//}/Lib/site-packages/mlflow/server/js/build/static/js/*.js.map'
 )
