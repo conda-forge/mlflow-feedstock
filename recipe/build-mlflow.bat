8895b292917e0c774c459eaea706379c77fb1b98
Uwe L. Korn <uwe.korn@quantco.com>
2021-05-17 16:59:16 +0200

    Rework recipe with more modern practices

--- recipe/build-mlflow.bat
+++ recipe/build-mlflow.bat
@@ -1,3 +1,6 @@
+@echo on
 %PREFIX%/python.exe -m pip install . --no-deps --ignore-installed -vv
+if [%PKG_NAME%] == [mlflow] (
 bash -c 'rm ${PREFIX//\\\\//}/Lib/site-packages/mlflow/server/js/build/static/css/*.css.map'
 bash -c 'rm ${PREFIX//\\\\//}/Lib/site-packages/mlflow/server/js/build/static/js/*.js.map'
+)
