cef45ab6f1562b0648ee36c894ad4b9afb32ae0a
Uwe L. Korn <uwe.korn@quantco.com>
2024-04-18 13:39:12 +0200

    Also adjust the windows build

--- recipe/build-mlflow.bat
+++ recipe/build-mlflow.bat
@@ -1,9 +1,16 @@
 @echo on
 
+if not exist pyproject-full.toml (
+    copy pyproject.toml pyproject.full.toml
+)
+
 if [%PKG_NAME%] == [mlflow-skinny] (
   set MLFLOW_SKINNY=1
   # https://github.com/mlflow/mlflow/pull/4134
   copy %RECIPE_DIR%/README_SKINNY.rst %SRC_DIR%
+  copy pyproject.skinny.toml pyproject.toml
+) else (
+  copy pyproject.full.toml pyproject.toml
 )
 
 %PREFIX%/python.exe -m pip install . --no-deps --ignore-installed -vv
