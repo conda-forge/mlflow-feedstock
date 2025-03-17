4a48cacf67b4bfdd3eefc0e435241a6cc736155f
TomuHirata <tomu.hirata@gmail.com>
2025-03-17 18:09:32 +0900

    import newly added packages
    Signed-off-by: TomuHirata <tomu.hirata@gmail.com>

--- recipe/build-mlflow.sh
+++ recipe/build-mlflow.sh
@@ -2,19 +2,6 @@
 
 set -euxo pipefail
 
-if [ ! -f pyproject.full.toml ]; then
-  cp pyproject.toml pyproject.full.toml
-fi
-
-if [[ "${PKG_NAME}" == "mlflow-skinny" ]]; then
-  export MLFLOW_SKINNY=1
-  # https://github.com/mlflow/mlflow/pull/4134
-  cp ${RECIPE_DIR}/README_SKINNY.rst ${SRC_DIR}
-  cp skinny/pyproject.toml pyproject.toml
-else
-  cp pyproject.full.toml pyproject.toml
-fi
-
 if [[ "${PKG_NAME}" == "mlflow-ui" ]]; then
   pushd mlflow/server/js
   yarn install
@@ -22,7 +9,15 @@ if [[ "${PKG_NAME}" == "mlflow-ui" ]]; then
   popd
 fi
 
-$PREFIX/bin/python -m pip install . --no-deps --ignore-installed --no-build-isolation -vv
+if [[ "${PKG_NAME}" == "mlflow-skinny" ]]; then
+  export MLFLOW_SKINNY=1
+  # https://github.com/mlflow/mlflow/pull/4134
+  cp ${RECIPE_DIR}/README_SKINNY.rst ${SRC_DIR}
+  $PREFIX/bin/python -m pip install skinny --no-deps --ignore-installed --no-build-isolation -vv
+else
+  $PREFIX/bin/python -m pip install . --no-deps --ignore-installed --no-build-isolation -vv
+fi
+
 if [[ "$PKG_NAME" != "mlflow-ui-dbg" ]]; then
   rm -rf $SP_DIR/mlflow/server/js/node_modules
   rm -f $SP_DIR/mlflow/server/js/build/static/css/*.css.map
