a20517d1dda40f2487e1a4db23e87744cd64c3f8
Weichen Xu <weichen.xu@databricks.com>
2025-06-30 13:13:03 +0800

    update
    Signed-off-by: Weichen Xu <weichen.xu@databricks.com>

--- recipe/build-mlflow.sh
+++ recipe/build-mlflow.sh
@@ -3,29 +3,27 @@
 set -euxo pipefail
 
 if [[ "${PKG_NAME}" == "mlflow-ui" ]]; then
-  pushd mlflow/server/js
 
   FILENAME="mlflow-${PKG_VERSION}.tar.gz"
   DOWNLOAD_URL="https://files.pythonhosted.org/packages/source/m/mlflow/${FILENAME}"
-  TMP_DIR="/tmp"
+  TMP_DIR="$(mktemp -d)"
   TAR_PATH="${TMP_DIR}/${FILENAME}"
   EXTRACT_DIR="${TMP_DIR}/mlflow-${PKG_VERSION}"
 
   echo "Downloading ${FILENAME}..."
-  curl -L -o "${TAR_PATH}" "${DOWNLOAD_URL}"
+  curl -fL -o "${TAR_PATH}" "${DOWNLOAD_URL}"
 
   echo "Extracting to ${EXTRACT_DIR}..."
   mkdir -p "${EXTRACT_DIR}"
   tar -xzf "${TAR_PATH}" -C "${EXTRACT_DIR}" --strip-components=1
 
   SRC_JS_BUILD="${EXTRACT_DIR}/mlflow/server/js/build"
-  DEST_DIR="$(pwd)/build"
+  DEST_DIR="mlflow/server/js/build"
 
   echo "Moving ${SRC_JS_BUILD} to ${DEST_DIR}..."
   rm -rf "${DEST_DIR}"
   mv "${SRC_JS_BUILD}" "${DEST_DIR}"
 
-  popd
 fi
 
 if [[ "${PKG_NAME}" == "mlflow-skinny" ]]; then
